14c1731077c6ba580c4bd37f326040e55e1d5f86
Andréa.C <155273473+Andie4@users.noreply.github.com>
2026-01-03 23:14:22 +0100

    stockage de la clé dans l'inventaire

--- Exercise/Assets/Scripts/ItemController.cs
+++ Exercise/Assets/Scripts/ItemController.cs
@@ -7,8 +7,10 @@ public class ItemController : TriggerController
     private static readonly int INVALID_ID = 0;
 
     [SerializeField] private GameObject m_Item;
+// --> pas trop compris pour le SerializeField 
 
     public int UniqueID { get; private set; } = INVALID_ID;
+    // pourquoi ne pas directement initailiser UniqueID à 0 ? 
 
     private void Awake()
     {
@@ -24,13 +26,20 @@ public class ItemController : TriggerController
         CanInteract = false;
     }
 
+// le code repris sur le repo est dans cet ordre mais il faudrait le mettre avant interact non ? Au dessus pickitem n'existe pas encore pourtant il est appelé 
     private void PickItem()
     {
         //TODO: Replace this with the correct implementation
-        throw new NotImplementedException("PickItem method is yet not implemented.");
+        // throw new NotImplementedException("PickItem method is yet not implemented.");
 
         //TODO: Store the item into the InventorySystem instance
+        InventorySystem.Instance.StoreItem(UniqueID);
+
         //TODO: Disable interaction from Trigger
+        CanInteract = false;
+
         //TODO: Deactivate item GameObject
+        m_Item.SetActive(false);
+
     }
 }
